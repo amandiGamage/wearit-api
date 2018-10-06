9eb82ff9534d50124cbf6b4558406f5de1ac76a4
Amandi Imasha <Amandiimasha43@gmail.com>
2018-10-06 18:22:13 +0530

    Item details dto added

---
+++ src/main/java/com/ijse/wearit/dto/ItemDetailsDTO.java
@@ -0,0 +1,57 @@
+package com.ijse.wearit.dto;
+
+import org.springframework.stereotype.Component;
+
+/**
+ *
+ * @author Amandi Gamage
+ */
+@Component
+public class ItemDetailsDTO extends SuperDTO{
+
+    private Integer _id;
+    private String _sizeName;
+    private String _itemDescription;
+    private double _unitPrice;
+    private int _qtyOnHand;
+
+    public Integer getId() {
+        return _id;
+    }
+
+    public void setId(Integer id) {
+        this._id = id;
+    }
+
+    public String getSizeName() {
+        return _sizeName;
+    }
+
+    public void setSizeName(String sizeName) {
+        this._sizeName = sizeName;
+    }
+
+    public String getItemDescription() {
+        return _itemDescription;
+    }
+
+    public void setItemDescription(String itemDescription) {
+        this._itemDescription = itemDescription;
+    }
+
+    public double getUnitPrice() {
+        return _unitPrice;
+    }
+
+    public void setUnitPrice(double unitPrice) {
+        this._unitPrice = unitPrice;
+    }
+
+    public int getQtyOnHand() {
+        return _qtyOnHand;
+    }
+
+    public void setQtyOnHand(int qtyOnHand) {
+        this._qtyOnHand = qtyOnHand;
+    }  
+}
