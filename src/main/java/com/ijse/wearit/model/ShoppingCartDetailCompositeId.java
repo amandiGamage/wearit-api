51a9cb3014512f30b3e7eb7c9b301dd135325261
Amandi Imasha <Amandiimasha43@gmail.com>
2018-10-06 16:23:25 +0530

    Shopping cart detail composite id added

---
+++ src/main/java/com/ijse/wearit/model/ShoppingCartDetailCompositeId.java
@@ -0,0 +1,48 @@
+package com.ijse.wearit.model;
+
+import java.io.Serializable;
+import javax.persistence.CascadeType;
+import javax.persistence.Embeddable;
+import javax.persistence.FetchType;
+import javax.persistence.ManyToOne;
+
+
+@Embeddable
+public class ShoppingCartDetailCompositeId  implements Serializable{
+    private ShoppingCart shoppingCart;
+    private ItemDetails itemDetals;
+    
+    public ShoppingCartDetailCompositeId(){
+        
+    }
+
+    /**
+     * @return the shoppingCart
+     */
+    @ManyToOne(cascade = CascadeType.ALL,fetch = FetchType.EAGER)
+    public ShoppingCart getShoppingCart() {
+        return shoppingCart;
+    }
+
+    /**
+     * @param shoppingCart the size to set
+     */
+    public void setShoppingCart(ShoppingCart shoppingCart) {
+        this.shoppingCart = shoppingCart;
+    }
+
+    /**
+     * @return the itemDetails
+     */
+    @ManyToOne(cascade = CascadeType.ALL,fetch = FetchType.EAGER)
+    public ItemDetails getItemDetails() {
+        return itemDetals;
+    }
+
+    /**
+     * @param itemDetails the item to set
+     */
+    public void setItemDetails(ItemDetails itemDetails) {
+        this.itemDetals = itemDetails;
+    }
+}
