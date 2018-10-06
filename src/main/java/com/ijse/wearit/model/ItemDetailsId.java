7b620b662697db87f1c55b0a8cf10ae162365386
Amandi Imasha <Amandiimasha43@gmail.com>
2018-10-06 16:20:53 +0530

    Item details id added

---
+++ src/main/java/com/ijse/wearit/model/ItemDetailsId.java
@@ -0,0 +1,43 @@
+package com.ijse.wearit.model;
+
+import java.io.Serializable;
+import javax.persistence.CascadeType;
+import javax.persistence.Embeddable;
+import javax.persistence.ManyToOne;
+
+
+@Embeddable
+class ItemDetailsId  implements Serializable{
+    private Sizes size;
+    private Item item;
+
+    /**
+     * @return the size
+     */
+    @ManyToOne(cascade = CascadeType.ALL)
+    public Sizes getSize() {
+        return size;
+    }
+
+    /**
+     * @param size the size to set
+     */
+    public void setSize(Sizes size) {
+        this.size = size;
+    }
+
+    /**
+     * @return the item
+     */
+    @ManyToOne(cascade = CascadeType.ALL)
+    public Item getItem() {
+        return item;
+    }
+
+    /**
+     * @param item the item to set
+     */
+    public void setItem(Item item) {
+        this.item = item;
+    }
+}
