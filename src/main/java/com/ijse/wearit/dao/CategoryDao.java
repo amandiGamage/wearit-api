86a1cf397ab78ab39111c8c0a8e3cf69b7c0ae22
Amandi Imasha <Amandiimasha43@gmail.com>
2018-10-06 17:55:48 +0530

    Updated

--- src/main/java/com/ijse/wearit/dao/CategoryDao.java
+++ src/main/java/com/ijse/wearit/dao/CategoryDao.java
@@ -4,5 +4,8 @@ import com.ijse.wearit.dao.custom.CategoryDaoCustom;
 import com.ijse.wearit.model.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import javax.transaction.Transactional;
+
+@Transactional
 public interface CategoryDao extends JpaRepository<Category,Integer>, CategoryDaoCustom {
 }
