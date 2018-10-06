1e56e7c1b4576a25754682d0cc7cba5a99eb218d
Amandi Imasha <Amandiimasha43@gmail.com>
2018-10-06 16:11:39 +0530

    Shipping info added

---
+++ src/main/java/com/ijse/wearit/model/ShippingInfo.java
@@ -0,0 +1,100 @@
+package com.ijse.wearit.model;
+
+import java.io.Serializable;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+
+@Entity
+public class ShippingInfo implements Serializable{
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)        
+    private Integer id;
+    
+    private String fName;
+    private String lName;
+    private String addressLine1;
+    private String city;
+    private String country;
+    private String postalCode;
+    private String contactNum;
+    
+    public ShippingInfo(){
+        
+    }
+
+    /**
+     * @return the id
+     */
+    public Integer getId() {
+        return id;
+    }
+
+    /**
+     * @param id the id to set
+     */
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getfName() {
+        return fName;
+    }
+
+    public void setfName(String fName) {
+        this.fName = fName;
+    }
+
+    public String getlName() {
+        return lName;
+    }
+
+    public void setlName(String lName) {
+        this.lName = lName;
+    }
+
+    public String getAddressLine1() {
+        return addressLine1;
+    }
+
+    public void setAddressLine1(String addressLine1) {
+        this.addressLine1 = addressLine1;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public String getPostalCode() {
+        return postalCode;
+    }
+
+    public void setPostalCode(String postalCode) {
+        this.postalCode = postalCode;
+    }
+
+    public String getContactNum() {
+        return contactNum;
+    }
+
+    public void setContactNum(String contactNum) {
+        this.contactNum = contactNum;
+    }
+    
+    
+    
+}
