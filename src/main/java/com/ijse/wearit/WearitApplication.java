5c7c750ce6eaaf86aa085b1ce7c6cb40954ad830
Amandi Imasha <Amandiimasha43@gmail.com>
2018-10-06 15:21:15 +0530

    Initial

---
+++ src/main/java/com/ijse/wearit/WearitApplication.java
@@ -0,0 +1,12 @@
+package com.ijse.wearit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WearitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WearitApplication.class, args);
+	}
+}
---
+++ src/test/java/com/ijse/wearit/WearitApplicationTests.java
@@ -0,0 +1,16 @@
+package com.ijse.wearit;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class WearitApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
