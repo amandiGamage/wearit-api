a625c8be3b4be842ee5e112fee4db74ae5b8c1e5
Amandi Imasha <Amandiimasha43@gmail.com>
2018-10-07 21:35:21 +0530

    Updated

--- src/main/java/com/ijse/wearit/dao/ItemDetailsDao.java
+++ src/main/java/com/ijse/wearit/dao/ItemDetailsDao.java
@@ -1,11 +1,12 @@
 package com.ijse.wearit.dao;
 
 import com.ijse.wearit.dao.custom.ItemDaoCustom;
+import com.ijse.wearit.dao.custom.ItemDetailsDaoCustom;
 import com.ijse.wearit.model.ItemDetails;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import javax.transaction.Transactional;
 
 @Transactional
-public interface ItemDetailsDao extends JpaRepository<ItemDetails,Integer>, ItemDaoCustom {
+public interface ItemDetailsDao extends JpaRepository<ItemDetails,Integer>, ItemDetailsDaoCustom {
 }
