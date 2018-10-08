268bc9c83122fbbafb8dfedd5bcc5556543cefe1
Amandi Imasha <Amandiimasha43@gmail.com>
2018-10-08 15:13:08 +0530

    Updated

--- src/main/java/com/ijse/wearit/dao/OrdersDao.java
+++ src/main/java/com/ijse/wearit/dao/OrdersDao.java
@@ -1,9 +1,10 @@
 package com.ijse.wearit.dao;
 
+import com.ijse.wearit.model.Orders;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import javax.transaction.Transactional;
 
 @Transactional
-public interface OrdersDao extends JpaRepository<OrdersDao,Integer> {
+public interface OrdersDao extends JpaRepository<Orders,Integer> {
 }
