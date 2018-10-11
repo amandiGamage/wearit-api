a4ff09b2dc097e28c84d4610249419628e2bf35a
Amandi Imasha <Amandiimasha43@gmail.com>
2018-10-11 21:31:04 +0530

    custom update method deleted

--- src/main/java/com/ijse/wearit/dao/custom/ItemDaoCustom.java
+++ src/main/java/com/ijse/wearit/dao/custom/ItemDaoCustom.java
@@ -3,5 +3,6 @@ package com.ijse.wearit.dao.custom;
 import com.ijse.wearit.model.Item;
 
 public interface ItemDaoCustom {
-    public Item getItemByDescription(String description)throws Exception;
+    public Item getItemByDescription(String description)throws Exception;;
+    public boolean deleteItemByDescription(String description)throws Exception;
 }
