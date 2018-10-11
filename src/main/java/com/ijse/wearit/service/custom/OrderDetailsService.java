685e15cfc3415546ca877c280de97ec8150a0fcb
Amandi Imasha <Amandiimasha43@gmail.com>
2018-10-12 01:43:28 +0530

    order details service added

---
+++ src/main/java/com/ijse/wearit/service/custom/OrderDetailsService.java
@@ -0,0 +1,9 @@
+package com.ijse.wearit.service.custom;
+
+import com.ijse.wearit.dto.OrderDetailsDTO;
+import com.ijse.wearit.model.OrderDetails;
+import com.ijse.wearit.service.SuperService;
+
+public interface OrderDetailsService extends SuperService<OrderDetailsDTO>{
+    
+}
