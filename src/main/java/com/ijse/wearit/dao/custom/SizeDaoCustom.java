d542d2f351939df877017565a15477dd3b1f9a05
Amandi Imasha <Amandiimasha43@gmail.com>
2018-10-11 21:25:52 +0530

    custom update method deleted

--- src/main/java/com/ijse/wearit/dao/custom/SizeDaoCustom.java
+++ src/main/java/com/ijse/wearit/dao/custom/SizeDaoCustom.java
@@ -4,5 +4,4 @@ import com.ijse.wearit.model.Sizes;
 
 public interface SizeDaoCustom {
     public Sizes getSizeByName(String name)throws Exception;
-    public boolean updateSize(Sizes sizes)throws Exception;
 }
