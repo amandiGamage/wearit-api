0f6cd61882bd1683013eba7bf3abfcf27c0b66e4
Amandi Imasha <Amandiimasha43@gmail.com>
2018-10-12 15:57:54 +0530

    Size service added

---
+++ src/main/java/com/ijse/wearit/service/custom/SizeService.java
@@ -0,0 +1,9 @@
+package com.ijse.wearit.service.custom;
+
+import com.ijse.wearit.dto.SizeDTO;
+import com.ijse.wearit.model.Sizes;
+import com.ijse.wearit.service.SuperService;
+
+
+public interface SizeService extends SuperService<SizeDTO>{
+}
