0754249a37eaba9b21c4b61ec9f8d1c05762ce44
Amandi Imasha <Amandiimasha43@gmail.com>
2018-10-12 15:14:35 +0530

    Payment service added

---
+++ src/main/java/com/ijse/wearit/service/custom/PaymentService.java
@@ -0,0 +1,9 @@
+package com.ijse.wearit.service.custom;
+
+import com.ijse.wearit.dto.PaymentDTO;
+import com.ijse.wearit.service.SuperService;
+
+
+public interface PaymentService extends SuperService<PaymentDTO>{
+    
+}
