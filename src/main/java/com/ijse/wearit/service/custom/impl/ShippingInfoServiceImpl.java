6e3d0b5d65141e5e6e5390db7b502a55f90040db
Amandi Imasha <Amandiimasha43@gmail.com>
2018-10-12 15:26:55 +0530

    Shipping info service add method implemented

--- src/main/java/com/ijse/wearit/service/custom/impl/ShippingInfoServiceImpl.java
+++ src/main/java/com/ijse/wearit/service/custom/impl/ShippingInfoServiceImpl.java
@@ -1,14 +1,28 @@
 package com.ijse.wearit.service.custom.impl;
 
+import com.ijse.wearit.dao.ShippingInfoDao;
 import com.ijse.wearit.dto.ShippingInfoDTO;
+import com.ijse.wearit.model.ShippingInfo;
 import com.ijse.wearit.service.custom.ShippingInfoService;
+import com.ijse.wearit.util.ModelConverter;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class ShippingInfoServiceImpl implements ShippingInfoService {
+
+    @Autowired
+    private ShippingInfoDao shippingInfoDao;
+
+    @Autowired
+    private ModelConverter modelConverter;
+
     @Override
     public ShippingInfoDTO add(ShippingInfoDTO shippingInfoDTO) throws Exception {
-        return null;
+        ShippingInfoDTO savedDTO = (ShippingInfoDTO) modelConverter.convertToDTO(
+                shippingInfoDao.save((ShippingInfo) modelConverter.convertToModel(
+                        shippingInfoDTO,ShippingInfo.class)),ShippingInfoDTO.class);
+        return savedDTO;
     }
 
     @Override
