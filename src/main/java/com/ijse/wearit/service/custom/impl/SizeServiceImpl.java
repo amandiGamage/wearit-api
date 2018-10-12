268b694b97d488c9fdddfcb5d618627eb7519c90
Amandi Imasha <Amandiimasha43@gmail.com>
2018-10-12 16:00:47 +0530

    Size service add and update methods implemented

--- src/main/java/com/ijse/wearit/service/custom/impl/SizeServiceImpl.java
+++ src/main/java/com/ijse/wearit/service/custom/impl/SizeServiceImpl.java
@@ -1,19 +1,40 @@
 package com.ijse.wearit.service.custom.impl;
 
+import com.ijse.wearit.dao.SizesDao;
 import com.ijse.wearit.dto.SizeDTO;
+import com.ijse.wearit.model.Sizes;
 import com.ijse.wearit.service.custom.SizeService;
+import com.ijse.wearit.util.ModelConverter;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class SizeServiceImpl implements SizeService {
+
+    @Autowired
+    private SizesDao sizesDao;
+
+    @Autowired
+    private ModelConverter modelConverter;
+
     @Override
     public SizeDTO add(SizeDTO sizeDTO) throws Exception {
-        return null;
+        SizeDTO savedDTO = (SizeDTO) modelConverter.convertToDTO(
+                sizesDao.save((Sizes) modelConverter.convertToModel(sizeDTO,Sizes.class)),SizeDTO.class);
+        return savedDTO;
+
     }
 
     @Override
     public boolean update(SizeDTO sizeDTO) throws Exception {
-        return false;
+        SizeDTO updatedDTO = (SizeDTO) modelConverter.convertToDTO(
+                sizesDao.save((Sizes) modelConverter.convertToModel(sizeDTO,Sizes.class)),SizeDTO.class);
+        if(updatedDTO!=null){
+            return true;
+        }else {
+            return false;
+        }
+
     }
 
     @Override
