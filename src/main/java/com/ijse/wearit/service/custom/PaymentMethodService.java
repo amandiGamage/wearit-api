46f38a1c7583564c3ad54e60d1a7d244514e72fa
Amandi Imasha <Amandiimasha43@gmail.com>
2018-10-12 14:53:08 +0530

    Payment method service added

---
+++ src/main/java/com/ijse/wearit/service/custom/PaymentMethodService.java
@@ -0,0 +1,9 @@
+package com.ijse.wearit.service.custom;
+
+import com.ijse.wearit.dto.PaymentMethodDTO;
+import com.ijse.wearit.service.SuperService;
+
+
+public interface PaymentMethodService extends SuperService<PaymentMethodDTO>{
+    
+}
