b3396c872d013a341a7b7e675b57f76a1c0f7622
Amandi Imasha <Amandiimasha43@gmail.com>
2018-10-12 15:03:43 +0530

    Payment method service add and update methods implemented

--- src/main/java/com/ijse/wearit/service/custom/impl/PaymentMethodServiceImpl.java
+++ src/main/java/com/ijse/wearit/service/custom/impl/PaymentMethodServiceImpl.java
@@ -1,20 +1,40 @@
 package com.ijse.wearit.service.custom.impl;
 
+import com.ijse.wearit.dao.PaymentMethodDao;
 import com.ijse.wearit.dto.PaymentMethodDTO;
+import com.ijse.wearit.model.PaymentMethod;
 import com.ijse.wearit.service.custom.PaymentMethodService;
+import com.ijse.wearit.util.ModelConverter;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class PaymentMethodServiceImpl implements PaymentMethodService {
 
+    @Autowired
+    private PaymentMethodDao paymentMethodDao;
+
+    @Autowired
+    private ModelConverter modelConverter;
+
     @Override
     public PaymentMethodDTO add(PaymentMethodDTO paymentMethodDTO) throws Exception {
-        return null;
+        PaymentMethodDTO savedDTO = (PaymentMethodDTO)
+                modelConverter.convertToDTO(paymentMethodDao.save((PaymentMethod)
+                        modelConverter.convertToModel(paymentMethodDTO,PaymentMethod.class)),PaymentMethodDTO.class);
+        return savedDTO;
     }
 
     @Override
     public boolean update(PaymentMethodDTO paymentMethodDTO) throws Exception {
-        return false;
+        PaymentMethodDTO updatedDTO = (PaymentMethodDTO) modelConverter.convertToDTO(
+                paymentMethodDao.save((PaymentMethod) modelConverter.convertToModel(paymentMethodDTO,PaymentMethod.class)),PaymentMethodDTO.class);
+
+        if(updatedDTO!=null){
+            return true;
+        }else {
+            return false;
+        }
     }
 
     @Override
