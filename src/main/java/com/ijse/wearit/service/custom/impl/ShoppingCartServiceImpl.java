f83c309902a304ff541a7659b97129050c0457fe
Amandi Imasha <Amandiimasha43@gmail.com>
2018-10-12 15:43:12 +0530

    Shoppingcart service add method implemented

--- src/main/java/com/ijse/wearit/service/custom/impl/ShoppingCartServiceImpl.java
+++ src/main/java/com/ijse/wearit/service/custom/impl/ShoppingCartServiceImpl.java
@@ -1,14 +1,28 @@
 package com.ijse.wearit.service.custom.impl;
 
+import com.ijse.wearit.dao.ShoppingCartDao;
 import com.ijse.wearit.dto.ShoppingCartDTO;
+import com.ijse.wearit.model.ShoppingCart;
 import com.ijse.wearit.service.custom.ShoppingCartService;
+import com.ijse.wearit.util.ModelConverter;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class ShoppingCartServiceImpl implements ShoppingCartService {
+
+    @Autowired
+    private ShoppingCartDao shoppingCartDao;
+
+    @Autowired
+    private ModelConverter modelConverter;
+
     @Override
     public ShoppingCartDTO add(ShoppingCartDTO shoppingCartDTO) throws Exception {
-        return null;
+        ShoppingCartDTO savedDTO = (ShoppingCartDTO) modelConverter.convertToDTO(
+                shoppingCartDao.save((ShoppingCart) modelConverter.convertToModel(
+                        shoppingCartDTO,ShoppingCart.class)),ShoppingCartDTO.class);
+        return savedDTO;
     }
 
     @Override
