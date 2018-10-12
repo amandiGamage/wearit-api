c3e8d149314e9a9d17a7189fbab0240d27344b14
Amandi Imasha <Amandiimasha43@gmail.com>
2018-10-12 16:25:36 +0530

    User service getUserByName custom method updated

--- src/main/java/com/ijse/wearit/service/custom/UserService.java
+++ src/main/java/com/ijse/wearit/service/custom/UserService.java
@@ -6,6 +6,6 @@ import com.ijse.wearit.service.SuperService;
 
 
 public interface UserService extends SuperService<UserDTO>{
-    public  UserDTO getUserByNam(String userName)throws Exception;
+    public  UserDTO getUserByName(String userName)throws Exception;
     public ShoppingCartDTO getShoppingCartByUserId(Integer userId)throws Exception;
 }
