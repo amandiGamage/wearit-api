fd8b2ebfee6a1ea61c52c0742fd503cfcea3a32e
Amandi Imasha <Amandiimasha43@gmail.com>
2018-10-12 15:36:07 +0530

    Shoppingcart details service add and update methods implemented

--- src/main/java/com/ijse/wearit/service/custom/impl/ShoppingCartDetailsServiceImpl.java
+++ src/main/java/com/ijse/wearit/service/custom/impl/ShoppingCartDetailsServiceImpl.java
@@ -1,14 +1,25 @@
 package com.ijse.wearit.service.custom.impl;
 
+import com.ijse.wearit.dao.ShoppingCartDetailsDao;
 import com.ijse.wearit.dto.ShoppingCartDTO;
 import com.ijse.wearit.dto.ShoppingCartDetailsDTO;
+import com.ijse.wearit.model.ShoppingCartDetails;
 import com.ijse.wearit.service.custom.ShoppingCartDetailsService;
+import com.ijse.wearit.util.ModelConverter;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
 public class ShoppingCartDetailsServiceImpl implements ShoppingCartDetailsService {
+
+    @Autowired
+    private ShoppingCartDetailsDao shoppingCartDetailsDao;
+
+    @Autowired
+    private ModelConverter modelConverter;
+
     @Override
     public ShoppingCartDetailsDTO getByItemDetail(Integer id) throws Exception {
         return null;
@@ -26,12 +37,22 @@ public class ShoppingCartDetailsServiceImpl implements ShoppingCartDetailsServic
 
     @Override
     public ShoppingCartDetailsDTO add(ShoppingCartDetailsDTO shoppingCartDetailsDTO) throws Exception {
-        return null;
+        ShoppingCartDetailsDTO savedDTO = (ShoppingCartDetailsDTO) modelConverter.convertToDTO(
+                shoppingCartDetailsDao.save((ShoppingCartDetails) modelConverter.convertToModel(
+                        shoppingCartDetailsDTO,ShoppingCartDetails.class)),ShoppingCartDetailsDTO.class);
+        return savedDTO;
     }
 
     @Override
     public boolean update(ShoppingCartDetailsDTO shoppingCartDetailsDTO) throws Exception {
-        return false;
+        ShoppingCartDetailsDTO updatedDTO = (ShoppingCartDetailsDTO) modelConverter.convertToDTO(
+                shoppingCartDetailsDao.save((ShoppingCartDetails) modelConverter.convertToModel(
+                        shoppingCartDetailsDTO,ShoppingCartDetails.class)),ShoppingCartDetailsDTO.class);
+        if(updatedDTO!=null){
+            return true;
+        }else {
+            return false;
+        }
     }
 
     @Override
